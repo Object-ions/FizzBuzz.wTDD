12598c08f408aab6fc0e7b2abb55bdc4b140bb8e
object-ions <moshikoatia@gmail.com>
2023-11-08 12:13:08 -0800

    Write 4th test and pass - 'return FizzBuzz'

--- __tests__/fizzBuzz.test.js
+++ __tests__/fizzBuzz.test.js
@@ -13,9 +13,9 @@ describe('fizzBuzz', () => {
     expect(fizzBuzz(6)).toEqual('Fizz');
     expect(fizzBuzz(12)).toEqual('Fizz');
   });
-  it ('should return "Buzz" if divisible by 5', () => {
-    expect(fizzBuzz(5)).toEqual('Buzz');
-    expect(fizzBuzz(10)).toEqual('Buzz');
-    expect(fizzBuzz(20)).toEqual('Buzz');
+  it ('should return "FizzBuzz" if divisible by 5', () => {
+    expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    expect(fizzBuzz(30)).toEqual('FizzBuzz');
+    expect(fizzBuzz(45)).toEqual('FizzBuzz');
   });
 });
