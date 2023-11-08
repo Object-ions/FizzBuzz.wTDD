c79873bbc36649369a4a771621d59b9e2f3a8dfd
object-ions <moshikoatia@gmail.com>
2023-11-08 11:15:29 -0800

    Add and pass secondtest - return the number if not divisable by 3 or 5

--- __tests__/fizzBuzz.test.js
+++ __tests__/fizzBuzz.test.js
@@ -3,5 +3,9 @@ import {fizzBuzz}  from '../src/js/fizzBuzz.js';
 describe('fizzBuzz', () => {
   it ('should be a function', () => {
     expect(typeof fizzBuzz).toEqual('function');
-  })
+  });
+  it ('should return the number if not divisible by 3 or 5', () => {
+    expect(fizzBuzz(1)).toEqual(1);
+    expect(fizzBuzz(17)).toEqual(17);
+  });
 });
