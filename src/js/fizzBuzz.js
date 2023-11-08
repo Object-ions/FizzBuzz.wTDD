b55d3a0a4f7403604afda0566091731f93b91ccb
object-ions <moshikoatia@gmail.com>
2023-11-08 12:12:10 -0800

    Expand fx to include FizzBuzz case

--- src/js/fizzBuzz.js
+++ src/js/fizzBuzz.js
@@ -1,9 +1,11 @@
 export const fizzBuzz = (number) => {
-  if (number % 3 === 0) {
+  if (number % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (number % 3 === 0) {
     return 'Fizz';
   } else if (number % 5 === 0) {
     return 'Buzz';
   } else {
     return number;
   }
-}
+};
