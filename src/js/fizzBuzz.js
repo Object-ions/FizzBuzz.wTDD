861a432e93cdb1c602ee118e4bb795cc9d74f469
object-ions <moshikoatia@gmail.com>
2023-11-08 11:24:07 -0800

    Fix typo

--- src/js/fizzBuzz.js
+++ src/js/fizzBuzz.js
@@ -2,7 +2,7 @@ export const fizzBuzz = (number) => {
   if (number % 3 === 0) {
     return 'Fizz';
   } else if (number % 5 === 0) {
-    return 'buzz';
+    return 'Buzz';
   } else {
     return number;
   }
